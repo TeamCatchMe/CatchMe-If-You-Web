571ff1baef208045e2aba1da41517768e0591e8c
hoon <devkwonsehoon@naver.com>
2021-08-05 23:32:54 +0900

    update: for catchme

--- public/app.js
+++ public/app.js
@@ -1,9 +1,11 @@
+// welcome catchme
 const content = '다채로운 나를 찾을 준비가 되셨나요?\n캐치미로 초대합니다!';
 
 const text = document.getElementById('intro_text');
 let index = 0;
 let txt = '';
 
+// typing animation
 function typing() {
   if (index < content.length) {
     txt += content[index];
@@ -13,9 +15,9 @@ function typing() {
 }
 setInterval(typing, 100);
 
+// button smooth scroll animation
 let skipBtn = document.getElementById('button');
 let upBtn = document.getElementById('up_button');
-// go버튼 뿐 만아니라 다른 menu버튼에도 똑같이 부드러운 스크롤을 적용.
 
 skipBtn.addEventListener('click', function (ev) {
   ev.preventDefault();
